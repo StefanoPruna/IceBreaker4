611b4b4be01d8c19dfddef0e56dd8fdf951d2634
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2025-02-03 14:36:22 +1100

    Created Unreal Project

---
+++ IceBreaker5/Intermediate/Build/Win64/x64/IceBreaker5Editor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Users/User/Documents/GitHub/IceBreaker5/IceBreaker5/Intermediate/Build/Win64/x64/IceBreaker5Editor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ IceBreaker5/Source/IceBreaker5/IceBreaker5.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IceBreaker5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IceBreaker5, "IceBreaker5" );
